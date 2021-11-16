60da21c9ae784f7730971bdc65491509ff85560c
Jamie Flynt <jamflynt@gmail.com>
2021-11-16 01:38:12 -0500

    Created basic return functionality. Search creates a div populated with character stats.

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
+let search;
 async function starWarSearch() {
+    const name = document.createElement('p');
+    const birth_year = document.createElement('p');
+    const resultsDiv = document.querySelector(`.output-here`);
+    resultsDiv.classList.add(`result`);
     try {
         console.log(`GET successful`);
         const star1 = await axios.get(`https://swapi.dev/api/people/?search=${search}`);
         console.log(star1.data.results);
-
+        resultsDiv.innerHTML = 
+        `<div>
+        <p>Name: ${star1.data.results[0].name}</p>
+        <p>Birth Year: ${star1.data.results[0].birth_year}</p>
+        <p>Eye Color: ${star1.data.results[0].eye_color}</p>
+        <p>Weight: ${star1.data.results[0].mass} kg</p>
+        <p>Height: ${star1.data.results[0].height} cm</p>
+        </div>`;
     } catch (err){
         console.log(`GET failed`);
         console.log(err);
+        resultsDiv.innerHTML = 
+        `<div>
+            <p>Nothing found for "${search}."  Please try Another search term.</p>
+        </div>`;
     }
 }
-starWarSearch();
 
-let search;
-const resultDiv = document.querySelector(`.results`);
-const output = document.createElement('p');
-const body = document.querySelector(`body`);
-body.append(output);
 
 const form = document.querySelector(`form`)
 form.addEventListener(`submit`, (s) => {
